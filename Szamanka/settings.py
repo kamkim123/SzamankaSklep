d5a5e4b9c2e92cba29e2d16599ad03920454aab5
kamkim123 <kamyk3226@o2.pl>
2025-10-09 10:53:19 +0200

    Zmiana 4 url

--- Szamanka/settings.py
+++ Szamanka/settings.py
@@ -7,7 +7,7 @@ DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 SECRET_KEY = os.getenv("SECRET_KEY", "dber74gf7%&DFcwcwdq!")
 ALLOWED_HOSTS = [
     "api.szamanka.pl",             # Twoja subdomena API
-    "https://szamankasklep.onrender.com",      # chwilowo adres z Render, podmienisz po deployu
+    "szamankasklep.onrender.com",      # chwilowo adres z Render, podmienisz po deployu
     "localhost", "127.0.0.1"
 ]
 
