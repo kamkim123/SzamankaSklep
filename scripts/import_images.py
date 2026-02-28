6fd659d2509f43c3de8393577ae3e64ea8d62c0b
kamkim123 <kamyk3226@o2.pl>
2026-02-28 14:41:28 +0100

    Sklep linki do produktow i dodawanie zdjec na serwer test import zdjec 1.4

--- scripts/import_images.py
+++ scripts/import_images.py
@@ -4,18 +4,20 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Szamanka.settings")
 django.setup()
 
+import os
 import csv
 import requests
-import os
 
-os.makedirs("downloads", exist_ok=True)
+# folder docelowy - Pulpit
+downloads_dir = "/Users/lena/Desktop/downloads"
+os.makedirs(downloads_dir, exist_ok=True)
 
 with open("/Users/lena/Desktop/products-photos.csv", newline="", encoding="utf-8") as f:
     reader = csv.DictReader(f)
     for row in reader:
-        url = row["photo"]  # nazwa kolumny w CSV z linkami do zdjęć
+        url = row["photo"]  # nazwa kolumny z linkami w CSV
         filename = url.split("/")[-1]
-        filepath = os.path.join("downloads", filename)
+        filepath = os.path.join(downloads_dir, filename)
         r = requests.get(url)
         if r.status_code == 200:
             with open(filepath, "wb") as img_file:
